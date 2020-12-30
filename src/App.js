e60eac957c9638d2779da26434b296f3befca5bf
Tanner Marshall <tanner.s.marshall@gmail.com>
2020-12-30 17:00:00 -0500

    Added useDraggable and example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import useDraggable from './useDraggable';
+import { useRef, useState } from 'react';
 
 function App() {
+  const divRef = useRef();
+
+  const [translate, setTranslate] = useState({ x: 0, y: 0 });
+
+  console.log(translate)
+  const handleDrag = (e) => {
+    setTranslate({
+      x: translate.x + e.movementX,
+      y: translate.y + e.movementY,
+    });
+  }
+
+  useDraggable(divRef, [translate], {
+    onDrag: handleDrag,
+    onMouseDown: () => console.log('Moving')
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div ref={divRef} style={{ transform: `translateX(${translate.x}px) translateY(${translate.y}px)`}} >
+          <img  src={logo} className="App-logo" alt="logo" />
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/useDraggable.js
@@ -0,0 +1,55 @@
+import { useState, useEffect } from 'react';
+
+const useDraggable = (ref, deps = [], options) => {
+  const {
+    onMouseDown = () => {},
+    onMouseUp = () => {},
+    onMouseMove = () => {},
+    onDrag = () => {},
+  } = options;
+
+  const [isDragging, setIsDragging] = useState(false);
+
+  const handleMouseDown = (e) => {
+    setIsDragging(true);
+
+    onMouseDown(e);
+  };
+
+  const handleMouseUp = (e) => {
+    setIsDragging(false);
+
+    onMouseUp(e);
+  };
+
+  const handleMouseMove = (e) => {
+    e.preventDefault();
+
+    onMouseMove(e);
+
+    if (isDragging) {
+      onDrag(e);
+    }
+  };
+
+  useEffect(() => {
+    const element = ref.current;
+    if (element) {
+      element.addEventListener('mousedown', handleMouseDown);
+      element.addEventListener('mouseup', handleMouseUp);
+      element.addEventListener('mousemove', handleMouseMove);
+
+      return () => {
+        element.removeEventListener('mousedown', handleMouseDown);
+        element.removeEventListener('mouseup', handleMouseUp);
+        element.removeEventListener('mousemove', handleMouseMove);
+      };
+    }
+
+    return () => {};
+  }, [...deps, isDragging]);
+
+  return { isDragging };
+};
+
+export default useDraggable;
